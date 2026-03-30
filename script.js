61bd221e4cff22fa9f68b0bc00b81a5729a9e79f
cutejotsingh-ux <cutejotsingh@gmail.com>
2026-03-30 19:09:51 +0530

    Add files via upload
    Signed-off-by: cutejotsingh-ux <cutejotsingh@gmail.com>

---
+++ script.js
@@ -0,0 +1,16 @@
+// STREAM URL - yahan apni real radio stream URL lagayein
+const STREAM_URL = "https://stream.zeno.fm/ckbq1t5y2tzuv";
+
+// DOM elements
+const audio = document.getElementById('radioAudio');
+const playPauseBtn = document.getElementById('playPauseBtn');
+const playIcon = document.getElementById('playIcon');
+const volumeSlider = document.getElementById('volumeSlider');
+const rewindBtn = document.getElementById('rewindBtn');
+const forwardBtn = document.getElementById('forwardBtn');
+const currentTrackSpan = document.getElementById('currentTrack');
+const currentArtistSpan = document.getElementById('currentArtist');
+const streamBitrateSpan = document.getElementById('streamBitrate');
+const liveStatusSpan = document.getElementById('liveStatusText');
+
+// ... aap original code ka saara JavaScript yahan paste karein ...
